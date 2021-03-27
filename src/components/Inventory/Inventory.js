b8b0b345cd3e2d4d26dffa9ee7ad2e208edaa666
Nayemul <nayemulhasan2016@gmail.com>
2021-03-27 17:54:56 +0600

    add products

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import fakeData from '../../fakeData';
 
 const Inventory = () => {
+    const handleAddProduct =() =>{
+        fetch('http://localhost:5000/addProduct',{
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(fakeData)
+        })
+        .then(data =>{
+            console.log(data);
+        })
+    }
     return (
         <div>
             <h3>this is inventory components</h3>
+            <button onClick={handleAddProduct}>Add Product</button>
         </div>
     );
 };
